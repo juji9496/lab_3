13645cce6631206f722357965d3d63fb2b815f51
juji9496 <juji9496@colorado.edu>
2019-09-20 09:21:24 -0700

    sub optional script file

--- lab3_script.sh
+++ lab3_script.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # Authors : Zack Jake
-# Date: 1/1/2019
+# Date: 9/20/2019
 
 #Problem 1 Code:
 #Make sure to document how you are solving each problem!
-#echo "Input a file:"
-#read fle1
-#echo "Enter a input expression"
-#read exp1
-#egrep $exp1 $fle1
-#egrep -c "[0-9]{3}-[0-9]{3}-[0-9]{4}" regex_practice.txt
-#egrep -c "303-[0-9]{3}-[0-9]{4}" regex_practice.txt
+echo "Input a file:"
+read fle1
+echo "Enter a input expression"
+read exp1
+egrep -c $exp1 $fle1
+egrep -c "[0-9]{3}-[0-9]{3}-[0-9]{4}" regex_practice.txt
+egrep -c "@.+\." regex_practice.txt
+egrep -o "303-[0-9]{3}-[0-9]{4}" regex_practice.txt
 egrep -c "@geocities.com" regex_practice.txt >> email_results.txt
 
